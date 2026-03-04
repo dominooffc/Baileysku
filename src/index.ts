ede3b5ae10f7d7c751b08c9afc4b47ba8d098639
PlanetOffc <makcapenal@gmail.com>
2026-03-04 16:54:33 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+import * as chalk from "chalk";
+
+console.clear();
+
+console.log(chalk.cyanBright.bold(`
+█▀█ █░ ▄▀█ █▄░█ █▀▀ ▀█▀
+█▀▀ █▄ █▀█ █░▀█ ██▄ ░█░
+`));
+
+console.log(chalk.greenBright.bold("Selamat datang di planet Baileys!\n"));
+
+console.log(chalk.whiteBright("Owner   : ") + chalk.yellowBright("@planetoffc"));
+console.log(chalk.whiteBright("Version : ") + chalk.yellowBright("29.0.0"));
+console.log(chalk.whiteBright("Update  : ") + chalk.yellowBright("Fix Error Pairing"));
+
+console.log(chalk.gray("\n----------------------------------\n"));
+
+console.log(chalk.magentaBright.bold("Selamat menggunakan Baileys planet ya!\n"));
+
 import makeWASocket from './Socket/index'
 
 export * from '../WAProto/index.js'
